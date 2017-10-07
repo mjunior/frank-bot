8cd688caf3e26a7eec0e75424294bd4dd5cd0edc
mjunior <mauriciojs.junior@gmail.com>
2017-10-07 13:21:58 -0300

    created tests for help servce

---
+++ spec/services/help_service_spec.rb
@@ -0,0 +1,15 @@
+require_relative './../spec_helper.rb'
+
+describe HelpService do
+  describe '#call' do
+    it "Response have the main commands" do
+      response = HelpService.call()
+      expect(response).to match('help')
+      expect(response).to match('Adicione ao Faq')
+      expect(response).to match('Remova ID')
+      expect(response).to match('O que você sabe sobre X')
+      expect(response).to match('Pesquise a hashtag X')
+      expect(response).to match('Perguntas e Respostas')
+    end
+  end
+end
