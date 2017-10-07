a12b8476190cb267f53b4ae8024e12169f66bbec
mjunior <mauriciojs.junior@gmail.com>
2017-10-07 13:23:28 -0300

    created tests for faq create_service

---
+++ spec/services/faq/create_service_spec.rb
@@ -0,0 +1,46 @@
+require_relative './../../spec_helper.rb'
+
+describe FaqModule::CreateService do
+  before do
+    @company = create(:company)
+
+    @question = FFaker::Lorem.setence
+    @answer = FFaker::Lorem.setence
+    @hashtags = "#{FFaker::Lorem.word}, #{FFaker::Lorem.word}"
+  end
+
+
+  describe '#call' do
+    it 'Without hashtag params, will receive a error' do
+      @createService = FaqModule::CreateService.new({"question-original" => @question, "answer-original" => @answer})
+        
+      response = @createService.call()
+      expect(response).to match("Hashtag Obrigatória")
+    end
+
+    it 'With valid params, receive success message' do
+      @createService = FaqModule::CreateService.new({"question-original" => @question, "answer-original" => @answer, "hashtags-original" => @hashtags})
+
+      response = @createService.call()
+      expect(response).to match("Criado com sucesso")
+    end
+
+    it 'With valid params, find question and answer in database' do
+      @createService = FaqModule::CreateService.new({"question-original" => @question, "answer-original" => @answer, "hashtags-original" => @hashtags})
+     
+      response = @createService.call()
+      expect(Faq.last.question).to match(@question)
+      expect(Faq.last.answer).to match(@answer)
+    end
+
+    it "With valid params, hashtags are created" do
+      @createService = FaqModule::CreateService.new({"question-original" => @question, "answer-original" => @answer, "hashtags-original" => @hashtags})
+
+      response = @createService.call()
+      expect(@hashtags.split(/[\s,]+/).first).to match(Hashtag.first.name)
+      expect(@hashtags.split(/[\s,]+/).last).to match(Hashtag.last.name)
+    end
+
+  end
+  
+end
