34c8de2e26bbdc3b8a72e9f75e3fe1e49bc620fa
mjunior <mauriciojs.junior@gmail.com>
2017-10-08 17:48:09 -0300

    added webhook

--- app.rb
+++ app.rb
@@ -6,10 +6,26 @@ require 'byebug'
 require './config/database'
 
 Dir['./app/models/*.rb'].each {|model| require model } 
+Dir['./app/services/**/*.rb'].each {|file| require file }
 
 class App < Sinatra::Base
   get '/sinatra' do
-    'Hello World'
+    'Hello world Sinatra!'   
   end
 
+  post '/webhook' do
+    result = JSON.parse(request.body.read)["result"]
+    if result["contexts"].present?
+      response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
+    else
+      response = InterpretService.call(result["action"], result["parameters"])
+    end
+
+    content_type :json
+    {
+      "speech": response,
+      "displayText": response,
+      "source": "Slack"
+    }.to_json
+  end
 end
