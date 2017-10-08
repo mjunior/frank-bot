90183099460d35dd211cd80ce8dc6b00dc6f043e
mjunior <mauriciojs.junior@gmail.com>
2017-10-08 17:46:58 -0300

    added interpretService

---
+++ app/services/interpret_service.rb
@@ -0,0 +1,16 @@
+class InterpretService
+  def self.call action, params
+    case action
+      when "list", "search", "search_by_hashtag"
+        FaqModule::ListService.new(params, action).call()
+      when "create"
+        FaqModule::CreateService.new(params).call()
+      when "remove"
+        FaqModule::RemoveService.new(params).call()
+      when "help"
+        HelpService.call()
+      else
+        "Não compreendi o seu desejo"
+      end
+  end
+end
