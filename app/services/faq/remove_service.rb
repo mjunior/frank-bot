78746d152a1039aa02fcf48b10657a924f228de7
mjunior <mauriciojs.junior@gmail.com>
2017-10-08 17:17:35 -0300

    added listservice

---
+++ app/services/faq/remove_service.rb
@@ -0,0 +1,23 @@
+module FaqModule
+  class RemoveService
+    def initialize(params)
+      @company = Company.last
+      @faq = @company.faqs.where(id: params[:id]).last  
+    end
+
+    def call
+      return "Questão não encontrada" if @faq.nil?
+
+      Faq.transaction do
+        # Deleta as tags associadas que não estejam associadas a outros faqs
+        @faq.hashtags.each do |h|
+          if h.faqs.count <= 1
+            h.delete
+          end
+        end
+        @faq.delete
+        "Deletado com sucesso"
+      end
+    end
+  end
+end
