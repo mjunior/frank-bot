2f6dfc989fb2b54512a16a706985fb99d1533acb
mjunior <mauriciojs.junior@gmail.com>
2017-10-11 21:01:05 -0300

    Deploy

--- config/database.rb
+++ config/database.rb
@@ -21,12 +21,13 @@ configure :development do
 end
 
 configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
   set :database, {
-    adapter: 'postgresql',
-    encoding: 'utf8',
-    database: 'onebitbot_production',
-    pool: 5,
-    username: 'postgres',
-    host: 'postgres'
+    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:     db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
   }
 end
---
+++ db/seeds.rb
@@ -0,0 +1,1 @@
+Company.create name: 'FrankBot'
